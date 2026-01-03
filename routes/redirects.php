40c7a1802057b074e82dba6b8b4398bda4402174
Nico Deblauwe <nico@bluepundit.eu>
2026-01-03 19:23:25 +0100

    CONTENT :pencil: Add redirect for paasvakantie to Notion page

--- routes/redirects.php
+++ routes/redirects.php
@@ -7,3 +7,5 @@ Route::redirect('narwal', 'https://forms.gle/5czASUmWBRj3y2sS7');
 Route::redirect('hoepel', 'https://docs.google.com/spreadsheets/d/1GbpfLdcwIST57q05N3raOfhVNqoN_iyUpyrCwOghqMc/edit?usp=sharing');
 Route::redirect('dokter', 'https://gvhv.us13.list-manage.com/track/click?u=8dba74059457f715696c892d3&id=79f107df4e&e=0056dfa0a4');
 
+Route::redirect('paasvakantie', 'https://bluepundit.notion.site/Fuerteventura-paasvakantie-2dd28ba3235d801a8017d70190427372');
+
